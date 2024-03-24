06390c0ef7c7108ff09942b7a4aa76c146635abc
Syed Muhammad Suleman <sulemansyedmuhammad41@gmail.com>
2024-03-25 01:10:19 +0500

    first comment

---
+++ README.md
@@ -0,0 +1,1 @@
+"# last-number-guessing-game" 
---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+//computer generated number-
+//input number by user-done
+//compare
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+//console.log(randomNumber);
+const answer = await inquirer.prompt([
+  {
+    name: "guessNumber",
+    type: "number",
+    message: "Guess any number between 1 to 6:",
+  },
+]);
+if (answer.guessNumber === randomNumber) {
+  console.log("Congratulation! Your guessed number is correct.");
+} else {
+  console.log("Your guessed number is incorrect.");
+}
